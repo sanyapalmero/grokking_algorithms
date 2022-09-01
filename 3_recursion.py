c0642d2f1470204dfc6e51ad817735b95b1b0402
Alexandr Vagner <sanyapalmero@gmail.com>
2022-09-01 17:56:29 +0500

    Add recursion sum function

--- 3_recursion.py
+++ 3_recursion.py
@@ -5,3 +5,28 @@ def factorial(x: int) -> int:
 
 print(factorial(5))
 print(factorial(6))
+
+
+def recursion_sum(arr: list) -> int:
+    if len(arr) == 0:
+        return 0
+    
+    first_elem = arr[0]
+    arr.pop(0)
+    return first_elem + recursion_sum(arr)
+
+
+def recursion_sum2(arr: list) -> int:
+    if len(arr) == 0:
+        return 0
+    
+    return arr[0] + recursion_sum(arr[1:])
+
+
+def recursion_sum2_inline(arr: list) -> int:
+    return arr[0] + recursion_sum(arr[1:]) if len(arr) != 0 else 0
+
+
+print(recursion_sum([2, 4, 6]))
+print(recursion_sum2([2, 4, 6]))
+print(recursion_sum2_inline([2, 4, 6]))
